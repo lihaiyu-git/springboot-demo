0618a91cadf2998187c189a96196536c439b99c2
lihaiyu <lihaiyu@goldmantis.com>
2018-11-26 13:48:51 +0800

    first commit

---
+++ src/main/java/com/example/TestController.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${com.springboot.example.name}")
+    private String name;
+
+    @RequestMapping("/test")
+    public String test(){
+        return name;
+    }
+
+    @RequestMapping("/test1")
+    public String test1(){
+        return name;
+    }
+
+
+
+}
